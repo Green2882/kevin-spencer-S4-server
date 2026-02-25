6187107632f6f1f300f90fde167039ed7e7e23f1
BigSponce <BigSponce@gmail.com>
2026-02-25 15:35:07 -0330

    Added CRUD operations to AirportService.java. Started interface integration in AirportRestRepository.java.

--- src/main/java/com/keyin/airport/AirportRestRepository.java
+++ src/main/java/com/keyin/airport/AirportRestRepository.java
@@ -1,4 +1,16 @@
 package com.keyin.airport;
 
-public class AirportRestRepository {
+import com.keyin.airport.Airport;
+import com.keyin.city.City;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+
+@Repository
+public interface AirportRestRepository extends CrudRepository<Airport, Long> {
+    Airport getAirportsByCity(City city);
+
+    Optional<Airport> findAirportByCity(City city);
 }
