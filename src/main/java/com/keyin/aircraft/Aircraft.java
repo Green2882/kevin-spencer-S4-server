39394605fc1747ddddc2c5900fe1af532953a35c
BigSponce <BigSponce@gmail.com>
2026-02-25 12:42:15 -0330

    Set up base code for Airport.java

--- src/main/java/com/keyin/aircraft/Aircraft.java
+++ src/main/java/com/keyin/aircraft/Aircraft.java
@@ -1,4 +1,58 @@
 package com.keyin.aircraft;
 
 public class Aircraft {
+    private String id;
+    private String type;
+    private String airlineName;
+    private String numOfPassengers;
+
+    public Aircraft(String id, String type, String airlineName, String numOfPassengers) {
+        this.id = id;
+        this.type = type;
+        this.airlineName = airlineName;
+        this.numOfPassengers = numOfPassengers;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getAirlineName() {
+        return airlineName;
+    }
+
+    public String getNumOfPassengers() {
+        return numOfPassengers;
+    }
+
+    public void setNumOfPassengers(String numOfPassengers) {
+        this.numOfPassengers = numOfPassengers;
+    }
+
+    public void setAirlineName(String airlineName) {
+        this.airlineName = airlineName;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Aircraft{" +
+                "id='" + id + '\'' +
+                ", type='" + type + '\'' +
+                ", airlineName='" + airlineName + '\'' +
+                ", numOfPassengers='" + numOfPassengers + '\'' +
+                '}';
+    }
 }
+
