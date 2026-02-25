deb9f31b7de23aaa89e28af4c84dbafe041ec2bd
BigSponce <BigSponce@gmail.com>
2026-02-25 12:41:32 -0330

    Set up base code for Passenger.java

--- src/main/java/com/keyin/passenger/Passenger.java
+++ src/main/java/com/keyin/passenger/Passenger.java
@@ -1,4 +1,57 @@
 package com.keyin.passenger;
 
 public class Passenger {
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String phoneNum;
+
+    public Passenger(String id, String firstName, String lastName, String phoneNum) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNum = phoneNum;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    @Override
+    public String toString() {
+        return "Passenger{" +
+                "id='" + id + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", phoneNum='" + phoneNum + '\'' +
+                '}';
+    }
 }
