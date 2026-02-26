65104eff0d9ed2e7b644c367ed2b81e538709bb2
Kevin Green <Greenkevin28@icloud.com>
2026-02-26 14:48:14 -0330

    Set up the server application to start testing in postman

--- src/main/java/com/keyin/AirportServerApplication.java
+++ src/main/java/com/keyin/AirportServerApplication.java
@@ -1,4 +1,11 @@
 package com.keyin;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class AirportServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AirportServerApplication.class, args);
+    }
 }
