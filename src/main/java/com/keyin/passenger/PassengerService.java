06cdda418d8dce3fbbae84f57d10e42e648dd348
Kevin Green <Greenkevin28@icloud.com>
2026-02-26 15:10:43 -0330

    Removed the @Autowired passengerService as it was causing error when running server

--- src/main/java/com/keyin/passenger/PassengerService.java
+++ src/main/java/com/keyin/passenger/PassengerService.java
@@ -10,8 +10,6 @@ public class PassengerService {
 
     @Autowired
     private PassengerRestRepository passengerRestRepository;
-    @Autowired
-    private PassengerService passengerService;
 
     public Passenger createPassenger(Passenger city){
         return passengerRestRepository.save(city);
