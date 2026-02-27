4b1379ed2cfef6d2c6ca889547db99b8ace3f453
BigSponce <BigSponce@gmail.com>
2026-02-27 12:49:06 -0330

    Fixed @DeleteMapping and @PutMapping for AircraftController.java based off changes Kevin made to CityController.java.

--- src/main/java/com/keyin/aircraft/AircraftController.java
+++ src/main/java/com/keyin/aircraft/AircraftController.java
@@ -1,5 +1,6 @@
 package com.keyin.aircraft;
 
+import com.keyin.airport.Airport;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -34,23 +35,15 @@ public class AircraftController {
         return ResponseEntity.ok(aircraftService.createAircraft(aircraft));
     }
 
-    @DeleteMapping()
-    public ResponseEntity<String> deleteAircraftById(@RequestParam Long id){
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteAircraftById(@PathVariable Long id){
         aircraftService.deleteAircraftById(id);
         return ResponseEntity.ok("Aircraft with id " + id + " deleted successfully");
     }
 
-    @PutMapping("")
-    public ResponseEntity<String> put(@RequestParam Long id, @RequestBody Aircraft aircraft){
-
-        Optional<Aircraft> aircraftOptional = aircraftService.findAircraftById(id);
-
-        aircraftOptional.get().setType(aircraft.getType());
-        aircraftOptional.get().setAirlineName(aircraft.getAirlineName());
-        aircraftOptional.get().setAirlineName(aircraft.getAirlineName());
-
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateAircraft(@PathVariable Long id, @RequestBody Aircraft aircraft){
+        aircraftService.updateAircraft(id, aircraft); // saves
         return ResponseEntity.ok("Aircraft with id " + id + " updated successfully");
     }
-
-
 }
