90a2b26b044c46ef1487c33327b18caddbeaff15
Kevin Green <Greenkevin28@icloud.com>
2026-04-07 15:55:31 -0230

    Fixing commit issue

--- src/main/java/com/keyin/airline/Airline.java
+++ src/main/java/com/keyin/airline/Airline.java
@@ -1,4 +1,61 @@
-package com.keyin.airline;
+package com.keyin.Airline;
 
+import com.keyin.airport.Airport;
+import com.keyin.flight.Flight;
+import jakarta.persistence.*;
+
+@Entity
 public class Airline {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Airport airport;
+    private Flight flight;
+
+    public Airline(String name) {
+        this.name = name;
+    }
+
+    public Airline() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Airport getAirport() {
+        return airport;
+    }
+
+    public void setAirport(Airport airport) {
+        this.airport = airport;
+    }
+
+    public Flight getFlight() {
+        return flight;
+    }
+
+    public void setFlight(Flight flight) {
+        this.flight = flight;
+    }
+
+    @Override
+    public String toString() {
+        return "Airline{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/keyin/airline/AirlineController.java
+++ src/main/java/com/keyin/airline/AirlineController.java
@@ -1,4 +1,48 @@
-package com.keyin.airline;
+package com.keyin.Airline;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/airline")
+@CrossOrigin
 public class AirlineController {
+
+    @Autowired
+    private AirlineService airlineService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Airline>> getAirlineById(@PathVariable Long id){
+        return ResponseEntity.ok(AirlineService.findAirlineById(id));
+    }
+
+    @GetMapping("/requestparam")
+    public ResponseEntity<Optional<Airline>> getByIdRequestParameter(@RequestParam Long id){
+        return ResponseEntity.ok(AirlineService.findAirlineById(id));
+    }
+
+    @GetMapping()
+    public ResponseEntity<Iterable<Airline>> getAllAirlines(){
+        return ResponseEntity.ok(airlineService.findAllAirlines());
+    }
+
+    @PostMapping()
+    public ResponseEntity<Airline> createAirline(@RequestBody Airline airline){
+        return ResponseEntity.ok(airlineService.createAirline(airline));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteAirlineById(@PathVariable Long id){
+        airlineService.deleteAirlineById(id);
+        return ResponseEntity.ok("Airline with id " + id + " deleted successfully");
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateAirline(@PathVariable Long id, @RequestBody Airline airline){
+        airlineService.updateAirline(id, airline);
+        return ResponseEntity.ok("Airline with id " + id + " updated successfully");
+    }
 }
